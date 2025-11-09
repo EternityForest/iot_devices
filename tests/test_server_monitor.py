320f45c2ae268b95289e3e73ea11649e28bf0e76
Daniel Dunn <dannydunn@example.com>
2025-11-08 20:49:34 -0700

    Test for server monitor

---
+++ tests/test_server_monitor.py
@@ -0,0 +1,17 @@
+import time
+import stamina
+from iot_devices.host import get_class, create_device
+
+
+def test_demo_device():
+    data = {"type": "ServerMonitor", "target": "localhost"}
+
+    c = get_class(data)
+    my_device = create_device(c, "ADevice", data)
+    time.sleep(0.5)
+
+    for attempt in stamina.retry_context(on=AssertionError, attempts=20):
+        with attempt:
+            assert my_device.datapoints["status"]
+            assert isinstance(my_device.datapoints["status"], int | float)
+    my_device.close()
